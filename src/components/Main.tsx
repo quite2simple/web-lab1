24e39b2ac0b53423b8de365a31e505fc5d669445
quite2simple <simplechuck882@gmail.com>
2024-02-19 17:29:10 +0300

    Adding interactivity to Main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 
+interface DeepNumbers {
+    a: number;
+    b: number;
+}
 
 function Main() {
-    return(
+
+    const randomInt = (a: number, b: number): number => {
+        return Math.floor(Math.random() * (b - a + 1)) + a;
+    }
+
+    const generateDeepNumbers = (): DeepNumbers => {
+        const a = randomInt(-10000, 8);
+        const b = 9 - a;
+        return {
+            a: a,
+            b: b
+        };
+    }
+
+    const [deepNumbers, setDeepNumbers] = useState<DeepNumbers>(generateDeepNumbers());
+
+    return (
         <main>
             <p className="deep-text">
                 6 + 3 = 9
                 <br />
-                but so does 5 + 4
+                but so does {deepNumbers.a} + {deepNumbers.b}
             </p>
             <p className="deep-text">
                 The way you do things is not always the only way to do them.
@@ -14,6 +35,7 @@ function Main() {
             <p className="deep-text">
                 Respect other people's way of thinking.
             </p>
+            <button onClick={() => setDeepNumbers(generateDeepNumbers())}>Get other numbers</button>
         </main>
     )
 }
