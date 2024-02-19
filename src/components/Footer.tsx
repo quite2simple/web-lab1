058aa7802c0146bfb6600fce1c18a03a83f0889a
quite2simple <simplechuck882@gmail.com>
2024-02-19 16:43:57 +0300

    Minor change to footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer>
             <p>
                 This is a reference to <a href="https://www.reddit.com/r/im14andthisisdeep/comments/1au9a9r/it_all_adds_up_now/">this</a> post from <a href="https://www.reddit.com/r/im14andthisisdeep/">Reddit</a>
-                , but with more <span>d e e p</span> truth added to it
+                , but with more <span>d e e p</span> (and interactive) truth added to it
             </p>
         </footer>
     )
